642d3716e51f59e13ede24ea703ac5d96e28be2f
Jacob <jacob5holding@hotmail.com>
2017-09-25 12:19:26 +0100

    some formatting

--- BasicTasks/task2.py
+++ BasicTasks/task2.py
@@ -5,6 +5,6 @@ salary = 700000
 tax = salary*21/100
 net_salary = salary - tax
 print("Employee: ", employee)
-print("Salary: £", salary)
-print("Tax: £", tax)
+print("Salary: £%0.2f" % salary)
+print("Tax: £%0.2f" % tax)
 print("Net Pay = £%0.2f" % net_salary)
