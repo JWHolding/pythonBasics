492d94922d72568d5ed0f81f5cec2f449282408b
Jacob <jacob5holding@hotmail.com>
2017-09-26 16:35:49 +0100

    started using a linter (why?!)

--- BasicTasks/timesTables.py
+++ BasicTasks/timesTables.py
@@ -1,5 +1,10 @@
+"""Script for doing times tables.
+
+Insert the incrementer and max value.
+"""
+
 mult = int(input("Which times tables do you want?:"))
 maxi = int(input("How many increments do you want?:"))
 
-for i in range(1,maxi+1):
+for i in range(1, maxi+1):
     print(i, "x", mult, "=", mult*i)
