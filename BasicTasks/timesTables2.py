9e5b08f052f6fdc7301ea0b0d96887fae001a06a
Jacob <jacob5holding@hotmail.com>
2017-09-27 10:16:02 +0100

    added variable times tables

--- BasicTasks/timesTables2.py
+++ BasicTasks/timesTables2.py
@@ -4,9 +4,10 @@ Calculate times tables between inputted number and 10 up to the increments
 """
 
 mult = int(input("Which times tables do you want to start with?:"))
+const = int(input("What is the max times table you want?:"))
 maxi = int(input("How many increments do you want?:"))
 
-for x in range(mult, 10):
+for x in range(mult, const + 1):
     print("Time Table of:", x)
     for i in range(1, maxi+1):
         print(i, "x", x, "=", x*i)
