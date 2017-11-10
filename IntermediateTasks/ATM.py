c23d8321c7fa6d2b390e2cfeb95434ed2feb5a0c
Jacob <jacob5holding@hotmail.com>
2017-11-10 15:33:04 +0000

    clears screen after taking user ID

--- IntermediateTasks/ATM.py
+++ IntermediateTasks/ATM.py
@@ -4,6 +4,13 @@ By Jacob JWHolding
 10.11.17
 """
 
+import os
 from ATMFunction import displayMenu
 
+ID = int(input("Please Insert your ID: "))
+
+if os.name == 'nt':
+    os.system('cls')
+else:
+    os.system('clear')
 displayMenu()
