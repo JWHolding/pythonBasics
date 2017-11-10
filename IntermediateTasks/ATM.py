fbd707dd853ab268d285f0d4d3fa408302c8bd59
Jacob <jacob5holding@hotmail.com>
2017-11-10 15:25:40 +0000

    Started with basic hello world menu

---
+++ IntermediateTasks/ATM.py
@@ -0,0 +1,9 @@
+"""An example ATM application.
+
+By Jacob JWHolding
+10.11.17
+"""
+
+from ATMFunction import displayMenu
+
+displayMenu()
---
+++ IntermediateTasks/ATMFunction.py
@@ -0,0 +1,6 @@
+"""Funtions for the ATM App."""
+
+
+def displayMenu():
+    """Print out ATM Menu."""
+    print("Hello World")
