bb1d15078b2f33ffbce8b2587904fbfcae6783ec
Jacob Holding <jacob5holding@hotmail.com>
2017-11-12 19:13:30 +0000

    added static objects, messed with other objects

--- ATMApp/ATM.py
+++ ATMApp/ATM.py
@@ -5,7 +5,16 @@ By Jacob JWHolding
 """
 
 from ATMFunction import displayMenu, clearScreen
+from account import Account
+
+acc1 = Account(100, "Jacob")
+acc2 = Account(100, "Gareth")
+acc3 = Account(100, "Elliot")
+acc4 = Account(100, "Tadas")
 
 ID = int(input("Please Insert your ID: "))
 clearScreen()
 displayMenu()
+
+print("Hello %s. Your Current Balance is: %0.2f" % (acc1.getName(),
+                                                    float(acc1.getBalance())))
