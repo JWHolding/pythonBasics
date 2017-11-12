f86a73e4efb25af7c160244db61fc652b2b05942
Jacob Holding <jacob5holding@hotmail.com>
2017-11-12 15:02:43 +0000

    cleaned up clearing screen to a function

--- IntermediateTasks/ATM.py
+++ IntermediateTasks/ATM.py
@@ -4,13 +4,8 @@ By Jacob JWHolding
 10.11.17
 """
 
-import os
-from ATMFunction import displayMenu
+from ATMFunction import displayMenu, clearScreen
 
 ID = int(input("Please Insert your ID: "))
-
-if os.name == 'nt':
-    os.system('cls')
-else:
-    os.system('clear')
+clearScreen()
 displayMenu()
--- IntermediateTasks/ATMFunction.py
+++ IntermediateTasks/ATMFunction.py
@@ -4,3 +4,12 @@
 def displayMenu():
     """Print out ATM Menu."""
     print("Hello World")
+
+
+def clearScreen():
+    """Clear the screen. OS detection included."""
+    import os
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
