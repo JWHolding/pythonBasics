e6d3ab01a1a5eef905a26161a3da789fbe9e0828
Jacob Holding <jacob5holding@hotmail.com>
2017-11-14 18:25:03 +0000

    added pin to object declaration

--- ATMApp/ATM.py
+++ ATMApp/ATM.py
@@ -5,14 +5,35 @@ By Jacob JWHolding
 """
 
 import getpass
-from ATMFunction import displayMenu, clearScreen
 from account import Account
 
-acc1 = Account(100, "Jacob")
-acc2 = Account(100, "Gareth")
-acc3 = Account(100, "Elliot")
-acc4 = Account(100, "Tadas")
-acc5 = Account(100, "Shafeeq")
+
+def displayMenu():
+    """Print out ATM Menu."""
+    print("Welcome To QA Bank")
+    print("------------------------------")
+    print("Press 1 to see Current Balance")
+    print("Press 2 to withdraw Money")
+    print("Press 3 to Deposit Money")
+    print("q to quit")
+    selection = str(input("Selection: "))
+    return selection
+
+
+def clearScreen():
+    """Clear the screen. OS detection included."""
+    import os
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+acc1 = Account(100, "Jacob", "0000")
+acc2 = Account(100, "Gareth", "0000")
+acc3 = Account(100, "Elliot", "0000")
+acc4 = Account(100, "Tadas", "0000")
+acc5 = Account(100, "Shafeeq", "0000")
 
 while True:
     clearScreen()
