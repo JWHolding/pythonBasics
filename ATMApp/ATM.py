7193a93ecc6342b75c31715ddb2e15ba44ac61d5
Jacob Holding <jacob5holding@hotmail.com>
2017-11-14 18:25:36 +0000

    re-instated functions in seperate file

--- ATMApp/ATM.py
+++ ATMApp/ATM.py
@@ -6,28 +6,7 @@ By Jacob JWHolding
 
 import getpass
 from account import Account
-
-
-def displayMenu():
-    """Print out ATM Menu."""
-    print("Welcome To QA Bank")
-    print("------------------------------")
-    print("Press 1 to see Current Balance")
-    print("Press 2 to withdraw Money")
-    print("Press 3 to Deposit Money")
-    print("q to quit")
-    selection = str(input("Selection: "))
-    return selection
-
-
-def clearScreen():
-    """Clear the screen. OS detection included."""
-    import os
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
-
+from ATMFunction import displayMenu, clearScreen
 
 acc1 = Account(100, "Jacob", "0000")
 acc2 = Account(100, "Gareth", "0000")
