b2619552d25e271da1ad8088ed6239855424f720
Jacob Holding <jacob5holding@hotmail.com>
2017-11-14 18:49:57 +0000

    added checking pin against stored pin

--- ATMApp/ATM.py
+++ ATMApp/ATM.py
@@ -5,6 +5,7 @@ By Jacob JWHolding
 """
 
 import getpass
+import time
 from account import Account
 from ATMFunction import displayMenu, clearScreen
 
@@ -17,28 +18,39 @@ acc5 = Account(100, "Shafeeq", "0000")
 while True:
     clearScreen()
     ID = int(input("Please Insert your ID: "))
-    clearScreen()
-    pin = getpass.getpass()
-    clearScreen()
-    count = int(0)
-    while True:
-        uin = str(displayMenu())
-        if uin == "1":
-            print("Hello %s. Your Current Balance is: %0.2f"
-                  % (acc1.getName(), float(acc1.getBalance())))
-            input("Press any key to continue...")
-            clearScreen()
-        elif uin == "2":
-            acc1.withdraw(int(input("How much would you like to Withdraw: ")))
-            input("Press any key to continue...")
-            clearScreen()
-        elif uin == "3":
-            acc1.deposit(int(input("How much would you like to Deposit: ")))
-            input("Press any key to continue...")
-            clearScreen()
-        elif uin == "Q" or uin == "q":
-            break
+    attempts = int(0)
+    while attempts < 4:
+        clearScreen()
+        pin = getpass.getpass()
+        clearScreen()
+        if acc1.checkPin(pin):
+            attempts = 0
+            while True:
+                uin = str(displayMenu())
+                if uin == "1":
+                    print("Hello %s. Your Current Balance is: %0.2f"
+                          % (acc1.getName(), float(acc1.getBalance())))
+                    input("Press any key to continue...")
+                    clearScreen()
+                elif uin == "2":
+                    acc1.withdraw(int(input("How much would you like to Withdraw: ")))
+                    input("Press any key to continue...")
+                    clearScreen()
+                elif uin == "3":
+                    acc1.deposit(int(input("How much would you like to Deposit: ")))
+                    input("Press any key to continue...")
+                    clearScreen()
+                elif uin == "Q" or uin == "q":
+                    print("Goodbye!")
+                    time.sleep(2)
+                    attempts = 4
+                    break
+                else:
+                    clearScreen()
+                    print("Invalid option, try again.")
+                    continue
         else:
-            clearScreen()
-            print("Invalid option, try again.")
-            continue
+            attempts = attempts + 1
+    if attempts < 4:
+        print("Too many incorrect attempts")
+        input("press any key to continue...")
